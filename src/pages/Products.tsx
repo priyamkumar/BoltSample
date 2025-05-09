d494b880dd097618a13f7a28436a7b194629b3c4
PRIYAM <you@example.com>
2025-05-09 20:19:22 +0530

    image changes

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -48,7 +48,7 @@ const Products = () => {
             price: 49.99 + (i * 10),
             category,
             rating: 3.5 + (Math.random() * 1.5),
-            image: `https://images.pexels.com/photos/${310000 + (i * 223)}/pexels-photo-${310000 + (i * 223)}.jpeg`
+            image: `https://images.unsplash.com/photo-1612528443702-f6741f70a049?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
           };
         });
         
